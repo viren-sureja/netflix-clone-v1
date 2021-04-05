d6cee22f08cde9597fbeeaa1547318f5ea93a6b7
viren sureja <virensureja3@gmail.com>
2021-04-06 00:57:37 +0530

    photos fetched for thumbnail and bit styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import request from './request';
 
 function App() {
-	return <div className="App">hello world</div>;
+	return (
+		<div className="App">
+			<Row
+				title="NETFLIX ORIGINALS"
+				fetchUrl={request.fetchNetflixOriginals}
+			/>
+			<Row title="Trending Now" fetchUrl={request.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+			<Row title="Romantic Movies" fetchUrl={request.fetchRomanticMovies} />
+			<Row title="Documentries" fetchUrl={request.fetchDocumentaries} />
+		</div>
+	);
 }
 
 export default App;
