0f987a24e0ffa5e183cd88113e168cf0053e7e59
viren sureja <virensureja3@gmail.com>
2021-04-06 13:51:48 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Row from './Row';
 import request from './request';
 import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
 	return (
 		<div className="app">
-			{/* navbar */}
-			{/* banner */}
+			<Nav />
 			<Banner />
 			<Row
 				title="NETFLIX ORIGINALS"
